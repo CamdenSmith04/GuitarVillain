1dd340a344681e076c28f88206f00c72bde0399a
CamdenSmith04 <ctsmith0319@gmail.com>
2025-03-03 11:55:57 -0500

    List classes stubs implemented with UUID moved into project

--- src/main/java/com/model/LessonList.java
+++ src/main/java/com/model/LessonList.java
@@ -1,5 +1,46 @@
 package com.model;
+import java.util.ArrayList;
 
 public class LessonList {
+
+    private static LessonList lessonList;
+
+    private ArrayList<Lesson> lessons;
+
+    private LessonList(){
     
+    }
+
+    public static LessonList getInstance(){
+        if (lessonList == null) {
+            lessonList = new LessonList();
+        }
+        return lessonList;
+    }
+
+    public void createLesson(Lesson lesson){
+        
+    }
+
+    public Lesson getLesson(String title){
+        // TODO: FIX
+        return null;
+    }
+
+    public void updateLesson(Lesson lesson){
+
+    }
+
+    public void deleteLesson(Lesson lesson){
+
+    }
+
+    public void save(){
+
+    }
+
 }
+
+
+
+
