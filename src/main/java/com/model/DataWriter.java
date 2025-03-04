8e4ea339a8871d77f8eeb3eb28f2023cc33f222a
keySmash13 <abigail.holdcraft@gmail.com>
2025-03-04 00:18:37 -0500

    reorganized and added DataConstants

---
+++ src/main/java/com/model/DataWriter.java
@@ -0,0 +1,16 @@
+package com.model;
+/**
+ * Writes data to json files
+ * @author Holdcraft
+ */
+public class DataWriter extends DataConstants {
+    public boolean saveSongs() {
+
+    }
+    public boolean saveUsers() {
+
+    }
+    public boolean saveModules() {
+        
+    }
+}
