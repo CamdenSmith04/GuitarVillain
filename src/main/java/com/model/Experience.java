d2b65f382cce7e0cdcca9e80d23d258f8ab918dd
keySmash13 <abigail.holdcraft@gmail.com>
2025-03-06 09:41:14 -0500

    added toString and getExperience methods

---
+++ src/main/java/com/model/Experience.java
@@ -0,0 +1,31 @@
+package com.model;
+
+import java.util.Enumeration;
+
+public enum Experience {
+    BEGINNER("Beginner"),
+    INTERMEDIATE("Intermediate"),
+    ADVANCED("Advanced");
+    
+    private final String label;
+    
+    private Experience(String label) {
+        this.label = label;
+    }
+
+    public String toString() {
+        return label;
+    }
+
+    public static Experience getExperience(String label) {
+        if (BEGINNER.label.equalsIgnoreCase(label))
+            return BEGINNER;
+        if (INTERMEDIATE.label.equalsIgnoreCase(label))
+            return INTERMEDIATE;
+        if (ADVANCED.label.equalsIgnoreCase(label))
+            return ADVANCED;
+        return null;
+    }
+
+
+}
