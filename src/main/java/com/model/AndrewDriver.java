6220784f3f1f0c45e5f19b4cde905ed9d844fd00
Andrew Goad <andregoadt33@gmail.com>
2025-03-18 20:51:02 -0400

    dataloader updated to use full user constructor

---
+++ src/main/java/com/model/AndrewDriver.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public class AndrewDriver {
+    public static void main(){
+        DataLoader dataLoader = new DataLoader();
+        System.out.println("Dataloader constructed");
+        
+    }
+}
