2e3786107ebe89bd99e631c17d9ff9f102c2bb03
bcalford <bcalford@email.sc.edu>
2025-03-24 03:08:05 -0400

    Fixes #33 Student Implementation

--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -1,25 +1,47 @@
 package com.model;
 
 import java.util.ArrayList;
-
+/**
+ * This class represents a student.
+ * @author Bradley Alford
+ */
 public class Student extends User{
-    
+
+    /**
+     * This is the list of courses of which the student is a member.
+     */
     private ArrayList<Course> courses;
 
+    /**
+     * This is a constructor for a new student.
+     * @param user The base user created before assigning as student.
+     * @param courses The list of courses for the student.
+     */
     public Student(User user, ArrayList<Course> courses) {
         super(user.getId(), user.getUsername(), user.getPassword(), user.getExperience(), user.getPoints(), user.getStreak(), user.getSecurityQuestion(), user.getSecurityAnswer(), user.getFriends(), user.getSongs());
         this.courses = courses;
-
     }
 
+    /**
+     * This begins and completes a lesson for a student.
+     * @param lesson The lesson for the student to complete.
+     */
     public void beginLesson(Lesson lesson) {
-
+        System.out.println(lesson.getEducationalMaterial());
+        System.out.println(lesson.getVisualAid());
+        lesson.setCompleted(true);
+        // TODO: Add more interactions once lesson is more figured out.
     }
 
+    /**
+     * This plays a song for a student.
+     * @param song The song to play.
+     */
     public void beginSong(Song song) {
-        
+        song.play();
     }
 
+    // Remove? Do we need one seperate from User's?
     public boolean isMatch(Course course) {
         return this.id.equals(course.getId());
     }
