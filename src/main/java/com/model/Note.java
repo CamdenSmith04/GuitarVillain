0ba51d3978db00e0b516c86a093b131809fb6784
Andrew Goad <andregoadt33@gmail.com>
2025-03-25 12:44:09 -0400

    added javadoc to time signature

--- src/main/java/com/model/Note.java
+++ src/main/java/com/model/Note.java
@@ -1,17 +1,41 @@
 package com.model;
 import org.jfugue.player.Player;
 
+/**
+ * This class represents a single note
+ * @author Andrew Goad
+ */
+
 public class Note {
-    
+    /**
+     * The time in the measure that the note should be played
+     */
     private int time;
+    /**
+     * String on the guitar associated with the note
+     */
     private char string;
+    /**
+     * Guitar fret number associated with the note
+     */
     private int fret;
+    /**
+     * Musical name of the note
+     */
     private String note;
+    /**
+     * Player object to play a ntoe
+     */
     private Player player;
 
-    
+    /**
+     * Array of all possible note names
+     */
     private static final String[] NOTES = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
-
+    /**
+     * Gets name of note, generated from fret and string
+     * @return musical name of the note
+     */
     public String getNoteName() {
         note = String.valueOf(string).toUpperCase();
         int baseIndex = findNoteIndex(note);
@@ -22,7 +46,11 @@ public class Note {
         int newIndex = (baseIndex + fret) % NOTES.length;
         return NOTES[newIndex];
     }
-
+    /**
+     * gets note from notes array
+     * @param note Name of the note
+     * @return array index of the note
+     */
     private static int findNoteIndex(String note) {
         for (int i = 0; i < NOTES.length; i++) {
             if (NOTES[i].equals(note)) {
@@ -31,7 +59,11 @@ public class Note {
         }
         return -1;
     }
-
+    /**
+     * converts flat notes to equivalent sharp notes
+     * @param note note name to be covnerted to flat
+     * @return converted note name
+     */
     private static String convertFlatToSharp(String note) {
         switch (note) {
             case "Db":
@@ -48,15 +80,18 @@ public class Note {
                 return note;
         }
     }
-    
-
-    public Note() {
-
-    }
-
+    /**
+     * plays note from name, using JFugue
+     */
     public void play(){
         player.play(this.getNoteName());
     }
+    /**
+     * Note constructor
+     * @param time time in mesure to be played at
+     * @param string guitar string name note is played on
+     * @param fret fret number note is played at
+     */
 
     public Note(int time, char string, int fret) {
         this.time = time;
@@ -65,26 +100,51 @@ public class Note {
         player = new Player();
     }
 
+    /**
+     * mutator for time
+     * @param time new time for note to be played at
+     */
+
     public void setTime(int time) {
         this.time = time;
     }
 
+    /**
+     * mutator for string
+     * @param string new string for ntoe to be played on
+     */
+
     public void setString(char string) {
         this.string = string;
     }
-
+    /**
+     * mutator for fret
+     * @param fret new fret for note to be played at 
+     */
     public void setFret(int fret) {
         this.fret = fret;
     }
-
+    /**
+     * accessor for time
+     * @return time in measure the note will be played at
+     */
     public int getTime() {
         return this.time;
     }
+
+    /**
+     * accessor for string
+     * @return Guitar string the note will be played on
+     */
 
     public char getString() {
         return this.string;
     }
 
+    /**
+     * Accessor for fret
+     * @return Guitar fret that the note will be playe at
+     */
     public int getFret() {
         return this.fret;
     }
--- src/main/java/com/model/TimeSignature.java
+++ src/main/java/com/model/TimeSignature.java
@@ -1,23 +1,45 @@
 package com.model;
-
+/**
+ * @author Andrew Goad
+ * This class represents a time signature
+ */
 public class TimeSignature {
-    
+    /**
+     * The top number in the time signature, or the number of beats in a measure of the song
+     */
     private final int topNumber;
+    /**
+     * The bottom number in the time signature, or the value of each beat in the song 
+     */
     private final int bottomNumber;
 
+    /**
+     * TimeSignature constructor
+     * @param topNumber parameter for the top number 
+     * @param bottomNumber parameter for the bottom number
+     */
     public TimeSignature(int topNumber, int bottomNumber) {
         this.topNumber = topNumber;
         this.bottomNumber = bottomNumber;
     }
 
+    /**
+     * accessor for the top number 
+     * @return top number of time signature
+     */
     public int getTopNumber(){
         return topNumber;
     }
-
-    public int getBottomeNumber(){
+    /**
+     * accessur for bottom number
+     * @return bottom number of timesignature
+     */
+    public int getBottomNumber(){
         return bottomNumber;
     }
-
+    /**
+     * returns time signature as a string
+     */
     @Override
     public String toString(){
         return Integer.toString(topNumber) + "/" + Integer.toString(bottomNumber);
