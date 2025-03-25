68536ce080f71ca2c61fa179461507c2a6a81da0
CamdenSmith04 <ctsmith0319@gmail.com>
2025-03-25 12:30:54 -0400

    Fixing structure and updating driver

--- src/main/java/com/facade/AdminFacade.java
+++
@@ -1,9 +0,0 @@
-package com.facade;
-
-/**
- * Facade for admins - actions not accessible to the typical user.
- * @author Holdcraft
- */
-public interface AdminFacade extends TeacherFacade {
-    
-}
