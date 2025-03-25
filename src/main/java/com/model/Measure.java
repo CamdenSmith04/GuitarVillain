19c40d6e258219e90af0a478001d435ef94000a1
Andrew Goad <andregoadt33@gmail.com>
2025-03-25 12:20:07 -0400

    Measure.java javadoc

--- src/main/java/com/model/Measure.java
+++ src/main/java/com/model/Measure.java
@@ -1,36 +1,75 @@
 package com.model;
 
 import java.util.ArrayList;
-
+/**
+* @author Andrew Goad
+* This class represents a measure in a song
+*/
 public class Measure {
-    
+    /**
+    * length of the measure in eighth notes 
+    */    
     private int length;
+    /**
+     * array of chords to be played in the measure
+     */
     private ArrayList<Chord> chords;
 
+    /**
+     * Measure constructor with length and chords
+     * @param length length of the measure in eighth notes
+     * @param chords arraylist of chords to be played in the measure
+     */
     public Measure(int length, ArrayList<Chord> chords) {
         this.length = length;
         this.chords = chords;
     }
 
+    /**
+     * Adds a chord to chords arraylist
+     * @param chord chord to be added
+     */
     public void addChord(Chord chord) {
         this.chords.add(chord);
     }
 
+    /**
+     * removes a chord from chords array
+     * @param chord chord to be removed
+     */
     public void removeChord(Chord chord) {
         this.chords.remove(chord);
     }
 
+    /**
+     * accessor for length
+     * @return lenght of measure in eighth notes
+     */
+
     public int getLength() {
         return this.length;
     }
+    /**
+     * accessor for chords arraylist
+     * @return ArrayList of chords in the measure
+     */
 
     public ArrayList<Chord> getChords() {
         return this.chords;
     }
 
+    /**
+     * mutator for length variable
+     * @param length new length
+     */
+
     public void setLength(int length) {
         this.length = length;
     }
+    /**
+     * mutator for chords arraylist
+     * @param chords new chords arraylist
+     */
 
     public void setChords(ArrayList<Chord> chords) {
         this.chords = chords;
