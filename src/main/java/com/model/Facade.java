25088ca26f25ff7ce9c12858a1f27357dc9c2774
keySmash13 <holdcraa@email.sc.edu>
2025-03-26 13:18:10 -0400

    Update facade

--- src/main/java/com/model/Facade.java
+++ src/main/java/com/model/Facade.java
@@ -75,7 +75,7 @@ public class Facade implements TeacherInterface {
     /**
      * Makes user a new student
      */
-    public void becomeStudent() {
+    public boolean becomeStudent() {
         if (!(currentUser instanceof Teacher) 
             && !(currentUser instanceof Student)) {
             ArrayList<UUID> courses = new ArrayList<>();
@@ -90,12 +90,14 @@ public class Facade implements TeacherInterface {
             this.currentUser = new Student(currentUser, courses);
             userList.updateUser(currentUser);
         }
+        return (this.currentUser.getClass().equals(Student.class));
     }
 
     /**
-     * Makes user a new teacher
+     * This method makes a user a teacher class.
+     * @return true or false if was successful
      */
-    public void becomeTeacher() {
+    public boolean becomeTeacher() {
         if (!(currentUser instanceof Teacher) 
             && !(currentUser instanceof Student)) {
 
@@ -112,35 +114,56 @@ public class Facade implements TeacherInterface {
             this.currentUser = new Teacher(currentUser, courses);
             userList.updateUser(currentUser);
         }
-    }
-
-    public void becomeAdmin() {
-        currentUser = new Admin(currentUser);
-        userList.updateUser(currentUser);
+        return (this.currentUser.getClass().equals(Teacher.class));
     }
 
     /**
-     * Logs user out
+     * Makes user an instance of Admin
      */
-    public void logout() {
-        DataWriter.save();
-        this.currentUser = null;
-        this.currentLesson = null;
-        this.currentSong = null;
-        this.currentModule = null;
-        this.currentCourse = null;
+    public boolean becomeAdmin() {
+        currentUser = new Admin(currentUser);
+        userList.updateUser(currentUser);
+        return true;
     }
 
+    /**
+     * This method logs the user out and clears all the stored information.
+     * @return true or false if logout was successful or not.
+     */
+    public boolean logout() {
+        try {
+            DataWriter.save();
+            this.currentUser = null;
+            this.currentLesson = null;
+            this.currentSong = null;
+            this.currentModule = null;
+            this.currentCourse = null;
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    /**
+     * This method calls the songList browse song method to allow the user to see all the songs.
+     */
     public void browseSongs() {
         songList.browseSongs();
     }
 
+    /**
+     * This method prints out all the lessons in the lesson list.
+     */
     public void browseLessons() {
         for (Lesson lesson : lessonList.getLessons()) {
             System.out.println(lesson);
         }
     }
 
+    /**
+     * This method prints out all the courses in the course lesson.
+     */
     public void browseMyCourses() {
         for (Course course : courseList.getCourses()) {
             System.out.println(course);
@@ -148,7 +171,7 @@ public class Facade implements TeacherInterface {
     }
 
     /**
-     * Displays all of currentUser's friends
+     * This method prints out all the friends a user has.
      */
     public void browseFriends() {
         System.out.println("Friends:");
@@ -157,19 +180,26 @@ public class Facade implements TeacherInterface {
         }
     }
 
-    // TODO: Should this be a lesson rather than string name
-    public void enterLesson(Lesson lesson) { 
-
-    }
-
+    /**
+     * This method allows the user to view a specific course.
+     * @param course the course the user will be able to view.
+     */
     public void viewCourse(Course course) {
         this.currentCourse = course;
     }
 
-    public void viewFriendProfile(User user) {
-        // display public friend user information by UUID?
+    /**
+     * This method will enable a user to view one of their friends profiles.
+     * @param user the friend who we will be viewing.
+     */
+    public void viewFriendProfile(User friend) {
+        
     }
 
+    /**
+     * This method enables the user to create a new song.
+     * @return the new song that was created.
+     */
     public void composeSong() {
         Song newSong = new Song(currentUser.getId());
         currentUser.addSong(newSong.getId());
@@ -177,6 +207,10 @@ public class Facade implements TeacherInterface {
         currentSong = newSong;
     }
 
+    /**
+     * Creates a new course
+     * @param name Name of the course
+     */
     public void makeCourse(String name) {
         ArrayList<UUID> teachers = new ArrayList<>();
         teachers.add(currentUser.getId());
@@ -185,118 +219,288 @@ public class Facade implements TeacherInterface {
         currentCourse = newCourse;
     }
    
+    /**
+     * Deletes a course
+     * @param name Name of the course
+     */
     public void deleteCourse(String name) {
         currentCourse = courseList.getCourseByName(name);
         courseList.deleteCourse(currentCourse);
     }
    
+    /**
+     * This method adds a student to the student list.
+     * @param student the student to be added.
+     */
     public void addStudent(UUID student) {
         currentCourse.addStudent(student);
-
     }
     
+    /**
+     * This method removes a student from the student list.
+     * @param student the student to be removed.
+     */
     public void removeStudent(UUID student) {
         currentCourse.removeStudent(student);
-
     }
     
+    /**
+     * This method adds a lesson to a course.
+     * @param lesson the lesson to be added.
+     */
     public void assignLesson(UUID lesson) {
         currentCourse.addLesson(lesson);
-
     }
     
+    /**
+     * This method removes a lesson from a course.
+     * @param lesson the lesson to be removed.
+     */
     public void removeLesson(UUID lesson) {
         this.currentCourse.removeLesson(lesson);
     }
     
+    /**
+     * This method adds a song to a course.
+     * @param song the song to be added.
+     */
     public void assignSong(UUID song) {
         this.currentCourse.addSong(song);
     }
     
+    /**
+     * This method removes a song from a course.
+     * @param song the song to be removed.
+     */
     public void removeSong(UUID song) {
         this.currentCourse.removeSong(song);
     }
-    // TODO: String name or course
+   
+    /**
+     * This method will allow the user to start a lesson.
+     * @param name the lesson that will be started.
+     */
     public void beginLesson(String name) {
-        // go to the start of the module? enter a lesson? not sure
+        
     }
-    // TODO: String name or course
+
+    /**
+     * This method allows the user to start a song by title.
+     * @param title the title of the song that will be started.
+     */
     public void beginSong(String title) {
         setCurrentSong(songList.getSongByTitle(title));
         currentSong.play();
     }
 
+    /**
+     * Prints a song to a file
+     * @param title Name of the song
+     * @param fileName Name of the file
+     */
+    public void printSongToFile(String title, String fileName){
+        songList.getSongByTitle(title).printToFile(fileName);
+    }
+    
+        /**
+     * Prints a song to a file
+     * @param id UUID of the song
+     * @param fileName Name of the file
+     */
+    public void printSongToFile(UUID id, String fileName){
+        songList.getSong(id).printToFile(fileName);
+    }
+
+    /**
+     * Prints a song to a file
+     * @param title Name of the song
+     */
+    public void printSongToFile(String title){
+        Song song = songList.getSongByTitle(title);
+        song.printToFile(song.getTitle() + ".txt");
+    }
+    
+    /**
+     * Prints a song to a file
+     * @param id Id of the song
+     */
+    public void printSongToFile(UUID id){
+        Song song = songList.getSong(id);
+        song.printToFile(song.getTitle() + ".txt");
+    }
+    
+    /**
+     * This method allows the user to start a song by UUID.
+     * @param id the id of the song that will be started.
+     */
     public void beginSong(UUID id) {
         setCurrentSong(songList.getSong(id));
         currentSong.play();
     }
 
+    /**
+     * This is a getter for the list of users.
+     * @return the list of users.
+     */
     public ArrayList<User> getUsers() {
         return userList.getUsers();
     }
 
-    public User getUser(UUID user) {
-        return userList.getUser(user);
+    /**
+     * Gets a user from the userlist by id
+     * @param userId Id of user
+     * @return User if found
+     */
+    public User getUser(UUID userId) {
+        return userList.getUser(userId);
     }
 
+    /**
+     * This is a getter for the list of songs.
+     * @return the list of songs.
+     */
     public ArrayList<Song> getSongs() {
         return songList.getSongs();
     }
 
-    public Song getSong(UUID song) {
-        return songList.getSong(song);
+    /**
+     * Gets a song from songlist by id
+     * @param songId Id of song
+     * @return Song if found
+     */
+    public Song getSong(UUID songId) {
+        return songList.getSong(songId);
     }
 
+    /**
+     * This is a getter for the list of modules.
+     * @return the list of modules.
+     */
     public ArrayList<Module> getModules() {
         return moduleList.getModules();
     }
 
+    /**
+     * This method gets a specific module from the module list.
+     * @param module the module being searched for.
+     * @return the module that is found.
+     */
     public Module getModuleById(UUID module) {
         return moduleList.getModuleById(module);
     }
 
+    /**
+     * This is a getter for the list of lessons.
+     * @return the list of lessons.
+     */
     public ArrayList<Lesson> getLessons() {
         return lessonList.getLessons();
     }
 
+    /**
+     * This method gets a specific lesson from the lesson list.
+     * @param lesson the lesson being searched for.
+     * @return the lesson that is found.
+     */
     public Lesson getLessonById(UUID lesson) {
         return lessonList.getLessonById(lesson);
     }
 
+    /**
+     * This is a getter for the list of courses.
+     * @return the list of courses.
+     */
     public ArrayList<Course> getCourses() {
         return courseList.getCourses();
     }
 
+    /**
+     * This method gets a specific course from the course list.
+     * @param course the course being searched for.
+     * @return the course that is found.
+     */
     public Course getCourseById(UUID course) {
         return courseList.getCourseById(course);
     }
 
+    /**
+     * This method is a getter for the current user.
+     * @return the current user.
+     */
     public User getCurrentUser() {
         return this.currentUser;
     }
 
+    /**
+     * This method is a getter for the current course.
+     * @return the current course.
+     */
     public Course getCurrentCourse() {
         return this.currentCourse;
     }
 
+    /**
+     * This method is a getter for the current lesson.
+     * @return the current lesson.
+     */
     public Lesson getCurrentLesson() {
         return this.currentLesson;
     }
 
+    /**
+     * This method is a getter for the current song.
+     * @return the current song.
+     */
     public Song getCurrentSong() {
         return this.currentSong;
     }
 
+    /**
+     * This method is a getter for the current module.
+     * @return the current module.
+     */
     public Module getCurrentModule() {
         return this.currentModule;
     }
 
+    /**
+     * This method is a setter for the current song.
+     * @param song the song that will be set as the current song.
+     */
     public void setCurrentSong(Song song) {
         this.currentSong = song;
     }
 
+    /**
+     * This method is a setter for the current course.
+     * @param course the course that will be set as the current course.
+     */
     public void setCurrentCourse(Course course) {
         this.currentCourse = course;
+    }
+
+    /**
+     * This method is a setter for the current module.
+     * @param module the module that will be set as the current module.
+     */
+    public void setCurrentModule(Module module) {
+        this.currentModule = module;
+    }
+
+    /**
+     * This method is a setter for the current user.
+     * @param user the user that will be set as the current user.
+     */
+    public void setCurrentUser(User user) {
+        this.currentUser = user;
+    }
+
+    /**
+     * This method is a setter for the current lesson.
+     * @param lesson the lesson that will be set as the lesson module.
+     */
+    public void setCurrentLesson(Lesson lesson) {
+        this.currentLesson = lesson;
     }
 
 }
