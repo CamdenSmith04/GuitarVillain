984c013f808fa0499775ab7961dff3a40982ebd5
keySmash13 <holdcraa@email.sc.edu>
2025-04-10 13:31:32 -0400

    implementing login GUI

--- src/main/java/com/music/LogInController.java
+++ src/main/java/com/music/LogInController.java
@@ -1,15 +1,11 @@
 package com.music;
 
 import java.io.IOException;
-import java.lang.classfile.Label;
 
 import com.model.Facade;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 
 public class LogInController {
 
@@ -23,9 +19,6 @@ public class LogInController {
     private void logIn() throws IOException {
         Facade facade = new Facade();
         
-        // username = userField.getText();
-        // password = pwordField.getText();
-
         if (facade.logIn(username.getText(), password.getText()))
             App.setRoot("home");
     }
