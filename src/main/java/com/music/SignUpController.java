8ede376106095ed941019c9a3494d8b5be86e449
keySmash13 <holdcraa@email.sc.edu>
2025-04-14 13:22:57 -0400

    implementing signup (unfinished)

--- src/main/java/com/music/SignUpController.java
+++ src/main/java/com/music/SignUpController.java
@@ -1,17 +1,22 @@
 package com.music;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.model.Experience;
 import com.model.Facade;
 import com.model.SecurityQuestion;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 /**
  * @author Abby Holdcraft
  */
-public class SignUpController {
+public class SignUpController implements Initializable {
 
     @FXML
     private TextField username;
@@ -20,23 +25,55 @@ public class SignUpController {
     private TextField password;
 
     @FXML
-    private TextField experience;
+    private ChoiceBox<String> experience;
 
-    @FXML
-    private TextField securityQuestion;
+    private String[] experienceLevels = {"Choose One", "Beginner", "Intermediate", "Advanced"};
 
-    @FXML
+    private String experienceData = null;
+
+    // @FXML
+    // private ChoiceBox<String> securityQuestion;
+
+    // private String[] securityQuestions = {"Choose One", "What was your first pet's name?",
+    //                                     "What is your mother's maiden name?",
+    //                                     "What was the name of your elementary school?"};
+    
+    // private String securityData = null;
+
     private TextField securityAnswer;
 
     @FXML
     private void signUp() throws IOException {
-        Facade facade = Facade.getInstance();
-        if (facade.signUp(username.getText(),
-                            password.getText(),
-                            Experience.getExperience(experience.getText()),
-                            SecurityQuestion.getSecurityQuestion(securityQuestion.getText()),
-                            securityAnswer.getText())) {
+        // Facade facade = Facade.getInstance();
+        // if (facade.signUp(username.getText(),
+        //     password.getText(),
+        //     Experience.getExperience(experienceData),
+        //     SecurityQuestion.getSecurityQuestion(securityData),
+        //     securityAnswer.getText())) {
             App.setRoot("home");
-        }
+        // }
     }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        experience.getItems().addAll(experienceLevels);
+        experience.setOnAction(this::setExperienceData);
+        // securityQuestion.getItems().addAll(securityQuestions);
+        // securityQuestion.setOnAction(this::setSecurityData);
+    }
+
+    @FXML
+    private void setExperienceData(ActionEvent event) {
+        experienceData = experience.getValue();
+        if (experienceData.equals("Choose One"))
+            experienceData = null;
+    }
+
+    // @FXML
+    // private void setSecurityData(ActionEvent event) {
+    //     securityData = securityQuestion.getValue();
+    //     if (securityData.equals("Choose One"))
+    //         securityData = null;
+    // }
+
 }
