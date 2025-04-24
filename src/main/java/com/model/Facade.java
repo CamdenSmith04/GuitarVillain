42f65f0851c95d9907ee76da32a7fb312cd5e1c8
keySmash13 <holdcraa@email.sc.edu>
2025-04-24 11:44:28 -0400

    saving after signup

--- src/main/java/com/model/Facade.java
+++ src/main/java/com/model/Facade.java
@@ -69,7 +69,6 @@ public class Facade implements TeacherInterface {
      */
     public boolean signUp(String username, String password, Experience experience, SecurityQuestion securityQuestion, String securityAnswer) {
         currentUser = userList.signUp(username, password, experience, securityQuestion, securityAnswer);
-        if (currentUser != null) userList.updateUser(currentUser);
         return currentUser != null;
     }
 
